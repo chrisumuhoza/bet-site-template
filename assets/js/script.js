94ee2478db7b451a7a09bc8efa8836b0fc52bdf4
chris <chrisumuhoza99@gmail.com>
2022-08-24 15:36:19 +0100

    Final work made on template before design

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+const sideMenu = document.querySelector(".aside");
+const menuBtn = document.querySelector(".icon-menu");
+const closeBtn = document.querySelector(".close-btn");
+const themeToggler = document.querySelector(".theme-toggler");
+
+menuBtn .addEventListener('click' , () => {
+    sideMenu.style.display = 'block';
+
+})
+
+
+closeBtn.addEventListener('click' , () =>{
+    sideMenu.style.display = 'none';
+
+})
+
+themeToggler.addEventListener('click' , () =>{
+    document.body.classList.toggle('');
+
+})
